25e10e7ef9d1c8f492309950c10dd147b6c6dd5a
NuhAli <50523069+NuhAli@users.noreply.github.com>
2020-09-03 19:13:17 +0100

    Add files via upload
    Project code uploaded

---
+++ build/precache-manifest.43600902e9db41c8b2062f064a03890b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba53c8708683737112d8ffe8b5fc173",
+    "url": "/ArkFitnessCalculator--Project-3/index.html"
+  },
+  {
+    "revision": "8883137d5fc82bdaa448",
+    "url": "/ArkFitnessCalculator--Project-3/static/css/main.8ada424c.chunk.css"
+  },
+  {
+    "revision": "dfc21b44de606ab72e6d",
+    "url": "/ArkFitnessCalculator--Project-3/static/js/2.be02f16b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ArkFitnessCalculator--Project-3/static/js/2.be02f16b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8883137d5fc82bdaa448",
+    "url": "/ArkFitnessCalculator--Project-3/static/js/main.230f36d0.chunk.js"
+  },
+  {
+    "revision": "2a12a94c7f85f3f50b73",
+    "url": "/ArkFitnessCalculator--Project-3/static/js/runtime-main.6dcf53d6.js"
+  },
+  {
+    "revision": "616e185d8b1624ef8536ca253eb577dd",
+    "url": "/ArkFitnessCalculator--Project-3/static/media/arkveg.616e185d.jpg"
+  }
+]);
